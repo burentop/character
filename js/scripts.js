420280f099d31c88ffc577310cff929ff4fc8c69
Brent Perry <burentoperii@gmail.com>
2016-12-14 15:03:23 -0600

    complete logic for input answers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,37 @@ $(document).ready(function () {
   $(".profile form").submit(function (event) {
     event.preventDefault();
     var name = $("#name").val();
-    var gender = parseInt($("#gender").val());
-    var age = parseInt($("#age").val());
-    var justice = parseInt($("#justice").val());
+    var talking = parseInt($("#talking").val());
+    var reading = parseInt($("#reading").val());
+    var problem = parseInt($("#problem").val());
+    var better = parseInt($("#better").val());
+    var code = "";
     
     if ((!name)) {
       $(".input-name").addClass("has-error");
       $("#name-warning").text("Please enter your name.");
     } else {
-      if (age < 18) {
-        $("#political-name").text("Snot-nosed Crybaby");
-        $("#political-reason").text("I'm sorry, but your generation is all about me, me, me! Spells trouble for the future of our country.");
-    } else if (justice > 3) {
-        $("#political-name").text("Violent Liberal");
-        $("#political-reason").text("It's obvious from your Supreme Court preference that you're no fan of justice at all.")
-    } else if (justice > 1) {
-        $("#political-name").text("Hippie");
-        $("#political-reason").text("You have some redeemability, but not much common sense.");
-    } else {
-        $("#political-name").text("Rational Individual");
-        $("#political-reason").text("I honestly have no way of knowing what your political leanings are, based on these simple questions, but you seem relatively reasonable.")
+      if (talking === 0) {
+        code += "T";
+      } else {
+        code += "L";
+      }
+      if (reading === 0) {
+        code += "R";
+      }
+      if (problem === 0) {
+        code += "C";
+      } else {
+        code += "L";
+      }
+      if (better === 0) {
+        code += "P";
+      } else {
+        code += "I";
+      }
+      $("#character-name").text(code);
+      $("#character-reason").text("I really have no idea what that code means, but I am certain that it reflects your character.");
+      $(".results").show();
     }
-
-    $(".results").show();
-    }
-    
   });
 });
